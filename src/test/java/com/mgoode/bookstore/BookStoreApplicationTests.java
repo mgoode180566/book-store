39baf966b8f9d52216eb1a858268f549e34af4a8
Dell <michaelgoode66@googlemail.com>
2020-10-25 06:46:08 +0000

    hkjh

--- src/test/java/com/mgoode/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/mgoode/bookstore/BookStoreApplicationTests.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(classes = BookStoreApplication.class)
 @DataJpaTest
 class BookStoreApplicationTests {
 
