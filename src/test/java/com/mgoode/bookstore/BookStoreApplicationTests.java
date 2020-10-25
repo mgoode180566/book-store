07c3ee51f3cb3cc95bd4b5efeebf4da4cda53223
Dell <michaelgoode66@googlemail.com>
2020-10-25 06:36:42 +0000

    hkjh

--- src/test/java/com/mgoode/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/mgoode/bookstore/BookStoreApplicationTests.java
@@ -1,10 +1,41 @@
 package com.mgoode.bookstore;
 
+import com.mgoode.bookstore.model.Book;
+import com.mgoode.bookstore.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@DataJpaTest
 class BookStoreApplicationTests {
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	public void findByISBN() {
+
+		Book book = new Book();
+		book.setISBN("123456789");
+
+		entityManager.persist(book);
+		entityManager.flush();
+
+		Optional<Book> result = bookRepository.findByISBN("123456789");
+
+		assertEquals(result.get().getISBN(), book.getISBN());
+
+	}
 
 	@Test
 	void contextLoads() {
